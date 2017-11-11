fb793cc7c759b8aee3c90343c6652a3bc3b1bb9e
bitrent <kushkamisha@yahoo.com>
2017-11-11 10:54:38 +0200

    getContractBalance(), giveMoney(), replenish() works

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -19,50 +19,88 @@ import faucet_artifacts from '../../build/contracts/Faucet.json'
 
 var Faucet = contract(faucet_artifacts);
 
-window.replenish = function(amount) {
+function getContractBalance() {
+    Faucet.setProvider(web3.currentProvider);
     Faucet.deployed().then(function(contractInstance) {
-        contractInstance.replenish({from: web3.eth.accounts[0], value: web3.toWei(0.1)}).then(function(v) {
-            console.log(Number(v))
+        contractInstance.getContractBalance.call().then(function(v) {
+           $("#getContractBalance").html(web3.fromWei(v.toString()));
         });
     });
 }
 
-// window.giveMoney = function() {
-//     // timeRemain();
-//     try {
-//         let address = $("input[name=address]").val();
-//         alert(address);
-//         $("#msg").html("We send some money to your address. When miners confirm this transaction you'll see refreshed balance.");
-//         Faucet.deployed().then(function(contractInstance) {
-//             contractInstance.giveMoney({gas: 140000, from: address}).then(function() {
-//                 $("#msg").html("");
-//                 getContractBalance();
+function timeRemain() {
+    // let address = $("input[name=address]").val();
+    let address = '0x9eD53b5d4521cB05989Df7618A8e66FbFFCAe952';
+    Faucet.setProvider(web3.currentProvider); // important
+    Faucet.deployed().then(function(contractInstance) {
+        contractInstance.timeRemain({from: address}).then(function(v) {
+           return Number(v);
+        });
+    });
+}
+
+// function canGetMoney() {
+//     Faucet.setProvider(web3.currentProvider);
+//     let amount = 0, val = 0, flag = false;
+//     Faucet.deployed().then(function(contractInstance) {
+//         contractInstance.getContractBalance.call().then(function(v) {
+//             val = Number(v);
+//             contractInstance.getAmount.call().then(function(a) {
+//                 amount = Number(a);
+//                 if (val > amount + 140000) {
+//                     alert('here');
+//                     flag = true;
+//                 }
+//                 alert(flag);
 //             });
 //         });
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
-
-// function timeRemain() {
-//     let address = $("input[name=address]").val();
-//     Faucet.deployed().then(function(contractInstance) {
-//         contractInstance.timeRemain.call({from: address}).then(function(v) {
-//             alert(Number(v));
-//         });
+    
+//     alert(flag);
+//     return false;
 //     });
 // }
 
-function getContractBalance() {
-  Faucet.setProvider(web3.currentProvider);
-  Faucet.deployed().then(function(contractInstance) {
-    contractInstance.getContractBalance.call().then(function(v) {
-      $("#getContractBalance").html(web3.fromWei(v.toString()));
-    });
-  });
+window.replenish = function(amount) {
+    try {
+        if (amount == -1) {
+            amount = $("input[name=amount]").val();
+            amount = amount.replace(',', '.');
+            if (isNaN(parseFloat(amount))) {
+                alert('Incorrect repelnish amount!');
+            }
+        }
+        amount = Number(amount);
+
+        let address = $("input[name=send-address]").val();
+
+        $("#msg").html("Thank you for replenish faucet's wallet. The balance will increase as soon as the transaction is recorded on the blockchain. Please wait.");
+        Faucet.deployed().then(function(contractInstance) {
+            contractInstance.replenish({from: address, value: web3.toWei(amount)}).then(function() {
+                $("#msg").html("");
+                getContractBalance();
+            });
+        });
+    } catch(err) {
+        console.log(err);
+    }
 }
 
-$( document ).ready(function() {
+window.giveMoney = function() {
+    try {
+        let address = $("input[name=address]").val();
+        $("#msg").html("We send some money to your address. When miners confirm this transaction you'll see refreshed balance.");
+        Faucet.deployed().then(function(contractInstance) {
+            contractInstance.giveMoney({gas: 140000, from: address}).then(function() {
+                $("#msg").html("");
+                getContractBalance();
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+$(document).ready(function() {
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source like Metamask")
     // Use Mist/MetaMask's provider
@@ -72,6 +110,6 @@ $( document ).ready(function() {
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
-
+  alert(timeRemain());
   getContractBalance();
 });
